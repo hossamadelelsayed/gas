960df45ece8799b79d165626e2c7722310fbd63b
HeshamEG <heshamsalama1212901022@gmail.com>
2017-08-22 17:58:01 +0200

    getting email
    email fiched while user login using his phone no

--- src/pages/registermember/registermember.ts
+++ src/pages/registermember/registermember.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import{MainPage} from "../main/main";
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import firebase from 'firebase';
 
 /**
  * Generated class for the RegistermemberPage page.
@@ -19,16 +20,42 @@ import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 export class RegistermemberPage {
 
   constructor(private authService:AuthServiceProvider,public navCtrl: NavController, public navParams: NavParams) {
+//registeration
 // this.authService.register("heshamsalama1212901022@gmail.com","123456")
 // .catch(error =>{
 //   console.log("errrr",error);
 //
 // });
-this.authService.doLogin("heshamsalama1212901022@gmail.com","123456").then(user=>{
-  console.log("user",user);
-  console.log("user",user.o);
+//login
+//sign in annonimously
+this.authService.doLogin("heshamsalama1212901022@gmail.com","11111111").then(user=>{
+let email=this.authService. getLogInEmail("0100");
+console.log("email returned using phone",email);
+  // this.authService.submitUserInfo("hesham","0100",user.uid,user.email);
 
+  // let rootRef = firebase.database().ref();
+
+// let adaRef = rootRef.child("users/ada").set("kkkk");
+
+//   console.log("user",user.uid);
+//    rootRef = firebase.database().ref("hello");
+//subscribe to a datasnapshot
+// rootRef.once("value")
+//   .then(function(snapshot) {
+  //   let key = snapshot.key; // null
+  //     console.log("errrr",key);
+  //
+  //   let firstName = snapshot.child("hello2").val(); // "Ada"
+  //   console.log("chiled",firstName);
+  //
+  // });
 });
+// this.authService.resetPassword("heshamsalama1212901022@gmail.com");
+
+//for logout
+
+// this.authService.doLogout();
+
   }
 
   ionViewDidLoad() {
--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -20,8 +20,51 @@ export class AuthServiceProvider {
     this.fireAuth = firebase.auth();
      this.userData = firebase.database().ref('/userData');
   }
+  //login by phone and password
+//friest getting email using phone no
+  getLogInEmail(phoneNo:any){
+
+    let emailRef = firebase.database().ref(phoneNo);
+
+
+    emailRef.once("value")
+      .then(function(snapshot) {
+
+        let firstName = snapshot.child(phoneNo).val(); // "Ada"
+
+        console.log("id     :  ",firstName);
+        return  firstName;
+
+      });
+
+  }
+  //send customer info to database authentication succses
+  submitUserInfo(name:any ,phoneNo :any,userId :any,email:any){
+    let rootRef = firebase.database().ref("customers/"+userId);
+
+rootRef.child("name").set(name);
+ rootRef.child("email").set(email);
+ rootRef.child("phoneNo").set(phoneNo);
+
+ let nameEmailRef = firebase.database().ref(phoneNo);
+nameEmailRef.child(phoneNo).set(email);
+  }
+  //signIn annonimously beforelogin
+  AnonymousSignIn(){
+    return this.fireAuth.signInAnonymously().catch(function(error) {
+     // Handle Errors here.
+     let errorCode = error.code;
+     let errorMessage = error.message;
+     console.log("error.message",error.message);
+     // ...
+   });
+  }
+  //login and returns err msg if err occare
   doLogin(email: string, password: string): any {
-    return this.fireAuth.signInWithEmailAndPassword(email, password);
+    return this.fireAuth.signInWithEmailAndPassword(email, password).catch(err=>
+    {
+      console.log("log in failed msg",err.message);
+    });
   }
   register(email: string, password: string): any {
   return this.fireAuth.createUserWithEmailAndPassword(email, password)
@@ -29,18 +72,26 @@ export class AuthServiceProvider {
       this.userData.child(newUser.uid).set({email: email});
     });
 }
+
+//resetPassword
 resetPassword(email: string): any {
-  return this.fireAuth.sendPasswordResetEmail(email);
+  return this.fireAuth.sendPasswordResetEmail(email).catch(err=>
+  {
+    console.log("log in failed msg",err.message);
+  });
 }
+//logout
 doLogout(): any {
   return this.fireAuth.signOut();
 }
 // redirect to Login Page if no user found/login.
-noUser(LoginPage : string){
-  firebase.auth().onAuthStateChanged(function(user) {
-  if (!user) {
-    // this.navCtrl.setRoot(LoginPage);
+noUser(userId :any){
+  if(userId==null){
+    console.log("user state msg","no user");
+return null;
+  }else{
+    return userId;
   }
-});
+
 }
 }
