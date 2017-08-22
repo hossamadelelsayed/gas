e6f51ba3921fbdcc624b3f47f17800ac7b226d33
HeshamEG <heshamsalama1212901022@gmail.com>
2017-08-22 13:57:08 +0200

    firebase
    auth using email and constructing the providers for both auth and database

--- src/pages/registermember/registermember.ts
+++ src/pages/registermember/registermember.ts
@@ -3,6 +3,8 @@ import { ConfirmPage } from './../confirm/confirm';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import{MainPage} from "../main/main";
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+
 /**
  * Generated class for the RegistermemberPage page.
  *
@@ -16,7 +18,17 @@ import{MainPage} from "../main/main";
 })
 export class RegistermemberPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private authService:AuthServiceProvider,public navCtrl: NavController, public navParams: NavParams) {
+// this.authService.register("heshamsalama1212901022@gmail.com","123456")
+// .catch(error =>{
+//   console.log("errrr",error);
+//
+// });
+this.authService.doLogin("heshamsalama1212901022@gmail.com","123456").then(user=>{
+  console.log("user",user);
+  console.log("user",user.o);
+
+});
   }
 
   ionViewDidLoad() {
