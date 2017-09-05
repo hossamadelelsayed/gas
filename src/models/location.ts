61a61d75c36cacdf9187fc8003bd76fa3aa59fc2
HeshamEG <heshamsalama1212901022@gmail.com>
2017-09-05 14:42:57 +0200

    modals

---
+++ src/models/location.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by a4p2 on 8/27/2017.
+ */
+export class Location {
+  constructor(
+    public lat : number ,
+    public lng : number ,
+    public label : string ,
+    public address : string = null
+  ){
+
+  }
+}
