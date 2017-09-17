17c191a28c4d82a33231df56b2b1b43157886491
Ahmedgomaaabdelrahman <a.gomaaabdelrahman@gmail.com>
2017-09-17 14:41:44 +0200

    aa

--- src/pages/termsandprivacy/termsandprivacy.ts
+++ src/pages/termsandprivacy/termsandprivacy.ts
@@ -1,24 +1,37 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the TermsandprivacyPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { Storage } from '@ionic/storage';
+import { AboutusProvider } from '../../providers/aboutus/aboutus';
 
 @Component({
   selector: 'page-termsandprivacy',
   templateUrl: 'termsandprivacy.html',
 })
 export class TermsandprivacyPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public Info : any ;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage,public about:AboutusProvider) {
+    this.about.arTermsAndConditions();
+    this.about.enTermsAndConditions();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TermsandprivacyPage');
+    this.InfoGas();
   }
-
+InfoGas(){
+  this.storage.get('lang').then((res)=>{
+    if(res=='en'){
+        this.about.enGetTermsAndConditions().then((res)=>{
+        this.Info=res.paragraph;
+        console.log(res);
+      });
+    }
+    else{
+      this.about.arGetTermsAndConditions().then((res)=>{
+        this.Info=res.paragraph;
+      console.log(res);
+      });
+    }
+    });
+    }
 }
