119e191c292bf4bafc8a2a047ea5dc256b0fe761
HeshamEG <heshamsalama1212901022@gmail.com>
2017-09-17 12:46:15 +0300

    about/main

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -43,8 +43,13 @@ export class MainPage {
     smsMessage:any;
     disId:string;
     hits = new BehaviorSubject([])
-    constructor(private callNumber: CallNumber,private sms: SMS,public geolocation: Geolocation,public navCtrl: NavController, public navParams: NavParams,
-                public menuCtrl: MenuController ,public distributor :DistributorProvider,    private authService:AuthServiceProvider) {
+    constructor(private callNumber: CallNumber,private sms: SMS
+                ,public geolocation: Geolocation,
+                public navCtrl: NavController,
+                public navParams: NavParams,
+                public menuCtrl: MenuController ,
+                public distributor :DistributorProvider,
+                private authService:AuthServiceProvider) {
 /// Reference database location for GeoFire
 
 
@@ -90,7 +95,7 @@ export class MainPage {
             // console.log('Error getting location', error);
         });
     }
-    
+
     ionViewDidLoad(){
         let self=this;
         this.sendCurrentLoc();
@@ -104,7 +109,7 @@ this.distributor.sendMyLoc(resp.coords.latitude, resp.coords.longitude);
 
       }).catch(err=>{
         self.setMarkers(err);
-        
+
       });     });
       // });
     }
@@ -120,8 +125,11 @@ this.distributor.sendMyLoc(resp.coords.latitude, resp.coords.longitude);
 
         //getting all keys (distribuotors IDs)
         self.markerRef.on('value', (snapshot)=> {
-          // self.markers=[];
-          //   console.log('changed loc','once');
+       //empty our
+          for (var i = 0; i < this.markers.length; i++) {
+            this.markers[i].setMap(null);
+          }
+          self.markers=new Array();
 
             //getting latlng for evry valid distributor
             snapshot.forEach(key => {
@@ -131,10 +139,10 @@ this.distributor.sendMyLoc(resp.coords.latitude, resp.coords.longitude);
 //getting latlng using geofire
                 this.geoFire.get(key.key).then(function (location) {
                     let latlng = new google.maps.LatLng(location[0],location[1]);
-                  
+
                         self.addMarker(latlng,key.key);
                         console.log('new marker',key.key);
-                   
+
 
                 }, (error)=> {
                     console.log("Error: " + error);
@@ -148,7 +156,7 @@ this.distributor.sendMyLoc(resp.coords.latitude, resp.coords.longitude);
         // Create a Firebase reference where GeoFire will store its information
                 this.navCtrl.push(CreateorderPage);
             }
-   
+
     current:any;
     toggleMenu()
     {
@@ -158,18 +166,24 @@ this.distributor.sendMyLoc(resp.coords.latitude, resp.coords.longitude);
     }
 
     addMarker(latlng:any,key:any){
+
       var marker = new google.maps.Marker({
             title:'oo'
             ,tag:key
       });
 
-        marker.setMap(this.map);
+        // marker.setMap(this.map);
         this.markers.push(marker);
-        for(let i=0;i<=this.markers.length;i++){
+        for(let i=0;i<this.markers.length;i++){
+
         if(this.markers[i].tag==key){
+        //   this.markers[i].setMap(null);
+
             this.markers[i].setPosition(latlng);
-            console.log('am in the if loop');
-            break;
+          this.markers[i].setMap(this.map);
+
+          console.log('am in the if loop',this.markers);
+            // break;
   }
 }
       google.maps.event.addListener(marker, 'click', () => {
--- src/providers/aboutus/aboutus.ts
+++ src/providers/aboutus/aboutus.ts
@@ -15,8 +15,10 @@ firebaseRef=firebase.database();
   constructor(public http: Http) {
     console.log('Hello AboutusProvider Provider');
   }
-  aboutUs(title:any,paragraph:any){
-    let about=this.firebaseRef.ref("about");
+  arAboutUs(){
+let title='عن غاز السعودية'    ;
+let paragraph='هذا التطبيق لخدمات توزيع انابيب الغاز بالمملكة العربية السعودية ';
+    let about=this.firebaseRef.ref("ar_about");
     about.once("value")
       .then(function(snapshot) {
         about.child('title').set(title);
@@ -25,8 +27,10 @@ about.child('paragraph').set(paragraph);
 
       });
   }
-  termsAndConditions(title:any,paragraph:any){
-    let about=this.firebaseRef.ref("terms");
+  arTermsAndConditions(){
+    let title='الشروط والاحكام'    ;
+    let paragraph='هذا التطبيق لخدمات توزيع انابيب الغاز بالمملكة العربية السعودية ';
+    let about=this.firebaseRef.ref("ar_terms");
     about.once("value")
       .then(function(snapshot) {
         about.child('title').set(title);
@@ -34,5 +38,72 @@ about.child('paragraph').set(paragraph);
         about.child('paragraph').set(paragraph);
 
       });
+  }
+  enAboutUs(){
+    let title='About GasKSa';
+    let paragraph='This App is for distributing of gas pips inside the KSA ';
+    let about=this.firebaseRef.ref("en_about");
+    about.once("value")
+      .then(function(snapshot) {
+        about.child('title').set(title);
+
+about.child('paragraph').set(paragraph);
+
+      });
+  }
+  enTermsAndConditions(){
+    let title='Terms And Conditions'    ;
+    let paragraph='This App is for distributing of gas pips inside the KSA ';
+    let about=this.firebaseRef.ref("en_terms");
+    about.once("value")
+      .then(function(snapshot) {
+        about.child('title').set(title);
+
+        about.child('paragraph').set(paragraph);
+
+      });
+  }
+  arGetAboutUs():Promise<any>{
+    let promise=new Promise((resolve, reject )=>{
+      let about=this.firebaseRef.ref("ar_about");
+about.once(('value'),snapshot=>{
+  console.log(snapshot)
+
+  resolve(snapshot)
+})
+    });
+  return promise;
+}
+  arGetTermsAndConditions():Promise<any>{
+    let promise=new Promise((resolve, reject )=>{
+      let about=this.firebaseRef.ref("ar_terms");
+      about.once(('value'),snapshot=>{
+        console.log(snapshot)
+
+        resolve(snapshot)
+      })
+    });
+    return promise;
+  } enGetAboutUs():Promise<any>{
+    let promise=new Promise((resolve, reject )=>{
+      let about=this.firebaseRef.ref("en_about");
+about.once(('value'),snapshot=>{
+  console.log(snapshot)
+
+  resolve(snapshot)
+})
+    });
+  return promise;
+}
+  enGetTermsAndConditions():Promise<any>{
+    let promise=new Promise((resolve, reject )=>{
+      let about=this.firebaseRef.ref("en_terms");
+      about.once(('value'),snapshot=>{
+        console.log(snapshot)
+        resolve(snapshot)
+
+      })
+    });
+    return promise;
   }
 }
